e5e89f670d297755d8f2c807552ed7cfae55604d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-08 02:53:48 +0900

    [add] mouseover/out(クラス変更)、on("click")クリック後の動き
    .clickも可能だけど、イベントが終了してしまい、他の動きが反映されないため("click",function)として渡されてる

--- app.js
+++ app.js
@@ -28,11 +28,33 @@
 
 
 // mouseoverイベント、青に変更
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").css({"background-color":"#0000FF"});
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").css({"background-color":"#FF0000"});
+//   });
+// });
+
+
+// mouseoverイベント、クラス変更
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").addClass("box1-ext");
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").removeClass("box1-ext");
+//   });
+// });
+
+
+// on(click)
 $(function(){
-  $(".box1").mouseover(function(){
-    $(".box1").css({"background-color":"#0000FF"});
+  $(".box1").on("click",function(){
+    $(".box1").addClass("box1-ext");
   });
   $(".box1").mouseout(function(){
-    $(".box1").css({"background-color":"#FF0000"});
+    $(".box1").removeClass("box1-ext");
   });
 });
