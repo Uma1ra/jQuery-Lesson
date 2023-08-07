cf274969fb19b13cd70ee195da18b8c7cafd2be8
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-08 01:21:40 +0900

    [add] 演習問題

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 //   $('.box1').slideDown(1000);
 // });
 
+// $(function(){
+//   $(".box1").slideUp(1000);
+// });
+
+// $(function(){
+//   $(".box1").show(1500);
+//   $(".box1").css({"background-color": "#0000FF"});
+// });
+
+// $(function(){
+//   $(".box1").hide(1500);
+// });
+
+
+// 演習問題
 $(function(){
-  $(".box1").slideUp(1000);
+  $(".box1").slideDown(1500,function(){
+    $(".box1").css({
+      "background-color": "#0000FF",
+      "height": "100px"
+    }).slideUp();
+  });
 });
