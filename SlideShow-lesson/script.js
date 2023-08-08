a57673fc79656a9ab16ce3ac4824bcc85ddeab8d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-09 01:18:04 +0900

    [fix] Swiperスペルミスが判明、修正済

--- SlideShow-lesson/script.js
+++ SlideShow-lesson/script.js
@@ -1,28 +1,12 @@
-// const swiper = new Swiper('.swiper', {
-//   loop: true,
-
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   navigation: {
-//     nextE1: '.swiper-button-next',
-//     prevE1: '.swiper-button-prev',
-//   }
-// });
-
 const swiper = new Swiper(".swiper", {
-  //オプションの設定
-  loop: true, //最後までスライドしたら最初の画像に戻る
- 
-  //ページネーション表示の設定
-  pagination: { 
-    el: ".swiper-pagination", //ページネーションの要素
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
   },
- 
-  //ナビゲーションボタン（矢印）表示の設定
-  navigation: { 
-    nextEl: ".swiper-button-next", //「次へボタン」要素の指定
-    prevEl: ".swiper-button-prev", //「前へボタン」要素の指定
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   }
 });
